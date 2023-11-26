9650b003a4c09396e80a1e0c50d6f7553e40e31c
potnysh <maxim.siverrr@gmail.com>
2023-11-26 20:07:02 +0200

    signup signin

--- js/signin.js
+++ js/signin.js
@@ -8,3 +8,44 @@
     saving to local storage
     redirection
 */
+const accountEmail = document.querySelector('#email');
+const accountPassword = document.querySelector('#password');
+
+
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  signin();
+})
+
+function signin(){
+
+  const token = window.localStorage.getItem('token');
+
+  fetch("http://localhost:8080/api/auth/signin/", {
+    method: 'POST',
+    body: JSON.stringify({
+    email: accountEmail.value,
+    password: accountPassword.value
+  }),
+    headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+    'Authorization': `Bearer ${token}`
+  },
+  })
+  .then ( response => {
+    if(response.ok){
+      localStorage.setItem('token', response.token)
+      window.location.href = '../index.html';
+    }
+    
+  })
+  .catch(err => console.error('Виникла помилка під час виконання запиту:', err))
+
+   
+ 
+
+
+
+
+}
--- js/signup.js
+++ js/signup.js
@@ -8,3 +8,49 @@
     saving to local storage
     redirection
 */
+
+const accountName = document.querySelector('#name');
+const accountEmail = document.querySelector('#email');
+const accountPassword = document.querySelector('#password');
+const accountRepeatPassword = document.querySelector('#repeat_password');
+
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  signup();
+})
+
+function signup(){
+
+if(!accountName.value || !accountEmail.value || !accountPassword.value){
+  console.warn("Заповніть всі поля");
+  return;
+}
+
+if(accountPassword.value !== accountRepeatPassword.value){
+  console.warn("Паролі не однакові")
+  return;
+}
+  console.log("sadfads")
+  fetch("http://localhost:8080/api/auth/signup/", {
+  method: 'POST',
+  body: JSON.stringify({
+    name: accountName.value,
+    email: accountEmail.value,
+    password: accountPassword.value,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+    'Authorization': `Bearer ${token}`
+  },
+})
+.then ( response => {return response.json()})
+.then (response => {
+  localStorage.setItem('token', response.token)
+  window.location.href = '../index.html';
+})
+.catch(err => console.error('Виникла помилка під час виконання запиту:', err))
+}
+ 
+
+
